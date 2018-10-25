68e0bee4f9b65df5213b1a3368ce1b5a6256222b
borjasegui <segui.borja@gmail.com>
2018-10-25 18:00:15 +0200

    javascript

---
+++ prueba1/cript.js
@@ -0,0 +1,14 @@
+var nombre = 'Borja';
+var apellido ='Segui';
+console.log('El nombre es:', nombre);
+console.log('El apellido es:', apellido);
+var nombreCompleto =nombre +' '+ apellido;
+
+console.log('El nombre completo es:',nombreCompleto);
+var numero = 4 + 7 + 9 + 2;
+console.log('El numero es :', numero)
+var numero2 = numero + 67;
+console.log('La suma total es :',numero,'+','67 =',numero2);
+
+var misnumeros = [ 1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+console.log ('mis numeros',misnumeros);
---
+++ prueba1/script2.js
@@ -0,0 +1,121 @@
+var meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+console.log('los meses del año pedidos son:', meses[0], ',', meses[6], ',', meses[10], 'y', meses[meses.length - 1])
+
+//if
+
+var estacachas = false;
+var peso = 40;
+var altura = 170;
+
+if (peso <= altura - 100 - 10) {
+    estacachas = false;
+    console.log('deja de ir al baño a vomitar')
+}
+else if (peso <= altura - 100) {
+    estacachas = true;
+    console.log('que cachas esta')
+}
+else {
+    estacachas = false;
+    console.log('ponte a trabajar PASIVA!!!!')
+}
+// tengo 3 numeros, muestre el mayor de los numeros
+
+var num1 = 120;
+
+var num2 = 3000;
+var num3 = 4500;
+
+//arreglalo borja!
+if (num1 >= num2) {
+    if (num1 >= num3) {
+        console.log('numero mayor es :', num1);
+    } else {
+        console.log('numero mayor es :', num3);
+    }
+}
+else {
+    console.log('numero mayor es :', num2);
+}
+
+//loops
+
+var misnumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'y', 't'];
+console.log(misnumeros);
+for (let index = 0; index < misnumeros.length; index++) {
+    const element = misnumeros[index];
+    console.log('los valores dados para', index, 'es', misnumeros[index]);
+}
+//tienda d mascotas,muestre 1000 mascotas
+
+var mascotitas = [];
+
+
+for (let index = 0; index < 20; index++) {
+    mascotitas[index] = 'perrito' + (index + 1);
+}
+
+mascotitas[995] = 'oveja negra';
+
+for (let index = 0; index < 20; index++) {
+    console.log('la mascota', index, 'se llama', mascotitas[index])
+
+}
+for (let index = 0; index < 20; index++) {
+    if (index == 0) {
+        console.log('el perro', (index + 1), 'no tiene hermanos')
+    } else {
+        console.log('perrito', (index + 1), 'es hermano de', (index - 1))
+
+    }
+}
+
+function generarnombreyapellido(nombre, apellido) {
+    let username = nombre;
+    let apell = apellido;
+    return username + ' ' + apell;
+}
+console.log(generarnombreyapellido('cristina', 'barriga'));
+
+
+
+
+
+function generaroperacion(num1, num2, op) {
+    let primernumero = num1;
+    let segundonumero = num2;
+
+    if (op == '*') {
+        return (num1 * num2)
+    }
+    else if (op == '/') {
+        if (num2 == 0)
+            return 0;
+        else {
+            return (num1 / num2)
+        }
+
+    }
+    else if (op == '+') {
+        return (num1 + num2)
+    }
+    else if (op == '-') {
+        return (num1 - num2);
+    }
+
+}
+console.log(generaroperacion(4, 8, '+'))
+console.log(generaroperacion(4, 8, '-'))
+console.log(generaroperacion(4, 8, '*'))
+console.log(generaroperacion(4, 8, '/'))
+console.log(generaroperacion(4, 0, '/'))
+
+
+
+function numarra(arrai, op) {
+    
+    for (let index = 0; index < arrai.length; index++) {
+    }
+    return index++
+}
+console.log(numarra([1, 2, 3, 4], '+'))
